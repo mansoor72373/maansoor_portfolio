c3b6d81397ddb3ee73fd15572f7f77aec4260a12
Mansoor Ahmad <123772973+mansoor72373@users.noreply.github.com>
2024-07-21 00:22:02 +0500

    git push origin main

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <div className="flex flex-col min-h-screen">
         <Navbar />
-        <div className="flex-1 p-6 mt-16"> 
+        <div className="flex-1 p-6 mt-16">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
